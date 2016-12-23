3bc92c18fcb32fd32698ea18bd42762e9826aa21
SaidSari <mustafa.sari@ug.bilkent.edu.tr>
2016-12-23 20:27:44 +0200

    Google Search

---
+++ Codes/Code-Parts/Google_Search.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        // Search Google for this.
+        SearchGoogle("bilkent cs319");
+    }
+
+    /// <summary>
+    /// Search Google for this term.
+    /// </summary>
+    static void SearchGoogle(string t)
+    {
+        Process.Start("http://google.com/search?q=" + t);
+    }
+}
